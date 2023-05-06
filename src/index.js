d06a363f5eb3fa0953cdf1053d6cb50ac3d44276
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-05-07 02:43:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import axios from 'axios';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
